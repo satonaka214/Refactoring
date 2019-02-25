f772084feb3b04ab974b30944527613a7cafff00
Satoshi Nakane <satoshi.nakane@a-saas.com>
2019-02-25 12:55:51 +0900

    リファクタリング後

--- src/main/java/chapter6/introduction_of_explanatory_variables/PlatformValidator.java
+++ src/main/java/chapter6/introduction_of_explanatory_variables/PlatformValidator.java
@@ -2,9 +2,11 @@ package chapter6.introduction_of_explanatory_variables;
 
 public class PlatformValidator {
     public void validate(String platform, String browser, int resize) {
-        if ((platform.toUpperCase().indexOf("MAC") > -1) &&
-                (browser.toUpperCase().indexOf("IE") > -1) &&
-                wasInitialized() && resize > 0) {
+        final boolean isMacOs = platform.toUpperCase().indexOf("MAC") > -1;
+        final boolean isIEBrowser = browser.toUpperCase().indexOf("IE") > -1;
+        final boolean wasResized = resize > 0;
+
+        if (isMacOs && isIEBrowser && wasInitialized() && wasResized) {
             System.out.println("true");
         } else {
             System.out.println("false");
